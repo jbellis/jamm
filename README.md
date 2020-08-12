b57e86fc93b2fc63cd141c507379ead1563a4c88
Robert Stupp <snazy@snazy.de>
2020-08-12 11:09:46 +0200

    Add microbenchmark support
    Adds the two jmh-dependencies, which then unfortunately also appear in the published pom.
    The microbenchmark class is also in the published jamm.jar, which isn't nice.

--- README.md
+++ README.md
@@ -11,6 +11,38 @@ Building
 "mvn package"; optionally, "mvn install"
 
 
+Benchmarks
+==========
+
+See [`Microbench.java`](./src/org/github/jamm/jmh/Microbench.java)
+
+Some microbenchmark results
+(JVM options: `-Xms16g -Xmx16g -XX:+UseG1GC -XX:+AlwaysPreTouch`):
+
+    Benchmark                        (guess)  (nested)  (refs)  Mode  Cnt    Score    Error  Units
+    Microbench.cls1              ALWAYS_SPEC       100       4  avgt    3    0.243 ±  0.170  us/op
+    Microbench.cls1            ALWAYS_UNSAFE       100       4  avgt    3    0.304 ±  0.078  us/op
+    Microbench.cls1                    NEVER       100       4  avgt    3    0.210 ±  0.126  us/op
+    Microbench.cls2              ALWAYS_SPEC       100       4  avgt    3    2.924 ±  0.272  us/op
+    Microbench.cls2            ALWAYS_UNSAFE       100       4  avgt    3    3.086 ±  0.419  us/op
+    Microbench.cls2                    NEVER       100       4  avgt    3    2.624 ±  0.131  us/op
+    Microbench.cls3              ALWAYS_SPEC       100       4  avgt    3    5.919 ±  1.433  us/op
+    Microbench.cls3            ALWAYS_UNSAFE       100       4  avgt    3    6.031 ±  1.632  us/op
+    Microbench.cls3                    NEVER       100       4  avgt    3    5.329 ±  0.513  us/op
+    Microbench.deeplyNested      ALWAYS_SPEC       100       4  avgt    3  500.569 ± 27.362  us/op
+    Microbench.deeplyNested    ALWAYS_UNSAFE       100       4  avgt    3  495.529 ± 66.984  us/op
+    Microbench.deeplyNested            NEVER       100       4  avgt    3  388.130 ± 83.949  us/op
+    Microbench.justByteArray     ALWAYS_SPEC       100       4  avgt    3    0.012 ±  0.011  us/op
+    Microbench.justByteArray   ALWAYS_UNSAFE       100       4  avgt    3    0.013 ±  0.004  us/op
+    Microbench.justByteArray           NEVER       100       4  avgt    3    0.055 ±  0.010  us/op
+    Microbench.justByteBuffer    ALWAYS_SPEC       100       4  avgt    3    0.942 ±  0.230  us/op
+    Microbench.justByteBuffer  ALWAYS_UNSAFE       100       4  avgt    3    0.935 ±  0.154  us/op
+    Microbench.justByteBuffer          NEVER       100       4  avgt    3    0.689 ±  0.247  us/op
+    Microbench.justString        ALWAYS_SPEC       100       4  avgt    3    0.616 ±  0.165  us/op
+    Microbench.justString      ALWAYS_UNSAFE       100       4  avgt    3    0.684 ±  0.636  us/op
+    Microbench.justString              NEVER       100       4  avgt    3    0.574 ±  0.098  us/op
+
+
 Use
 ===
 
---
+++ src/org/github/jamm/jmh/Microbench.java
@@ -0,0 +1,182 @@
+package org.github.jamm.jmh;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.github.jamm.MemoryMeter;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Threads(3)
+@Fork(value = 1, jvmArgsPrepend = {
+        "-javaagent:target/jamm-0.4.0-SNAPSHOT.jar",
+        "-Xms16g", "-Xmx16g",
+        "-XX:+UseG1GC",
+        "-XX:+AlwaysPreTouch"
+})
+@Warmup(iterations = 1, time = 5)
+@Measurement(iterations = 3, time = 1)
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class Microbench
+{
+    @Param({ "ALWAYS_SPEC", "ALWAYS_UNSAFE", "NEVER" })
+    private String guess;
+
+    private MemoryMeter meter;
+
+    private String valString;
+    private ByteBuffer heapByteBuffer;
+    private Cls1 cls1;
+    private Cls1 cls2;
+    private Cls1 cls3;
+    private byte[] bytes;
+    private long[] longs;
+    private Object[] objects;
+
+    private ClsX deeplyNested;
+    @Param({ "100" })
+    private int nested;
+    @Param({ "4" })
+    private int refs;
+
+    @Setup
+    public void setup()
+    {
+        MemoryMeter.Guess guess = MemoryMeter.Guess.valueOf(this.guess);
+        this.meter = new MemoryMeter().withGuessing(guess);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 300; i++)
+            sb.append('a');
+        valString = sb.toString();
+
+        heapByteBuffer = ByteBuffer.allocate(300);
+
+        cls1 = new Cls1();
+        cls2 = new Cls2();
+        cls3 = new Cls3();
+
+        bytes = new byte[200];
+        longs = new long[200];
+        objects = new Object[50];
+        for (int i = 0; i < objects.length; i++)
+            objects[i] = new Object();
+
+        ClsX last = null;
+        for (int i = 0; i < nested; i++)
+        {
+            ClsX o = new ClsX();
+            if (last == null)
+                deeplyNested = o;
+            else
+                last.objs.add(o);
+            last = o;
+
+            for (int j = 0; j < refs; j++)
+                o.objs.add(new ClsX());
+        }
+    }
+
+    //    @Benchmark
+    //    @Measurement(iterations = 1, time = 1)
+    //    public void baseline(Blackhole bh)
+    //    {
+    //        bh.consume(0);
+    //    }
+
+    @Benchmark
+    public void deeplyNested(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(deeplyNested));
+    }
+
+    @Benchmark
+    public void justString(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(valString));
+    }
+
+    @Benchmark
+    public void justByteBuffer(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(heapByteBuffer));
+    }
+
+    @Benchmark
+    public void justByteArray(Blackhole bh)
+    {
+        bh.consume(meter.measure(bytes));
+    }
+
+    //    @Benchmark
+    //    public void justLongArray(Blackhole bh)
+    //    {
+    //        bh.consume(meter.measure(longs));
+    //    }
+
+    //    @Benchmark
+    //    public void justObjectArray(Blackhole bh)
+    //    {
+    //        bh.consume(meter.measure(objects));
+    //    }
+
+    @Benchmark
+    public void cls1(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(cls1));
+    }
+
+    @Benchmark
+    public void cls2(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(cls2));
+    }
+
+    @Benchmark
+    public void cls3(Blackhole bh)
+    {
+        bh.consume(meter.measureDeep(cls3));
+    }
+}
+
+@SuppressWarnings("unused")
+class ClsX
+{
+    List<ClsX> objs = new ArrayList<>();
+}
+
+@SuppressWarnings("unused")
+class Cls1
+{
+    int i;
+    long l;
+}
+
+@SuppressWarnings("unused")
+class Cls2 extends Cls1
+{
+    Object ref = "foop" + System.nanoTime();
+    Object[] array = new Object[]{ "abc" + System.nanoTime(), "def" + System.nanoTime(), "ghi" + System.nanoTime() };
+}
+
+@SuppressWarnings("unused")
+class Cls3 extends Cls2
+{
+    Object more = new Cls2();
+}
+
