b8fc494801525c908b0a453911d4a49f27981f06
Dan Rusu <roosterdan@gmail.com>
2023-11-01 21:47:53 -0600

    Use random sizes for the collections according to the distribution
    70% will have between 0 and 100 elements
    20% between 100 and 700 elements
    10% between 700 and 1000 elements
    
    I also added a benchmark for linked lists in order to test deeply nested structures

--- test/org/github/jamm/jmh/BenchmarkMeasureCollections.java
+++ test/org/github/jamm/jmh/BenchmarkMeasureCollections.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -20,10 +21,19 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class BenchmarkMeasureCollections {
-
     public static final int NUM_COLLECTIONS_OF_EACH_TYPE = 100;
 
-    public static final int NUM_ELEMENTS_PER_COLLECTION = 1000;
+    /**
+     * The {@link #percentages} & {@link #numElementBoundaries} work together to define the distribution of
+     * size of the collections.
+     *
+     * In this case, we want the random collection sizes to have distribution:
+     * 70% between 0 and 100 elements
+     * 20% between 100 and 700 elements
+     * 10% between 700 and 1000 elements
+     */
+    public static final int[] percentages = {70, 20, 10};
+    public static final int[] numElementBoundaries = {100, 700, 1000};
 
     @Param({"INSTRUMENTATION", "INSTRUMENTATION_AND_SPECIFICATION", "SPECIFICATION", "UNSAFE"})
     private String guess;
@@ -32,13 +42,29 @@ public class BenchmarkMeasureCollections {
 
     private static ArrayList<Person>[] arrayLists;
 
-    private static HashMap[] hashMaps;
+    /**
+     * Measuring linked lists is special as it imitates measuring very deep object graphs.
+     */
+    private static LinkedList<Person>[] linkedLists;
+
+    private static HashMap<String, Person>[] hashMaps;
 
     static {
         try {
-            Random random = new Random();
+            validateConfigurationOfSizeDistribution();
+            // IMPORTANT: We must use a fixed seed so that multiple runs are comparable especially
+            // when comparing the performance impact of code changes. The size and contents of
+            // each collection is chosen randomly so a fixed seed guarantees that multiple runs
+            // are testing against the exact same data otherwise we might perceive an invalid
+            // performance improvement when in fact the data was easier to measure in the 2nd run
+            Random random = new Random(0);
 
             arrayLists = createArrayLists(random);
+
+            linkedLists = new LinkedList[arrayLists.length];
+            for (int i = 0; i < arrayLists.length; i++) {
+                linkedLists[i] = new LinkedList<Person>(arrayLists[i]);
+            }
 
             hashMaps = new HashMap[arrayLists.length];
             for (int i = 0; i < hashMaps.length; i++) {
@@ -57,34 +83,89 @@ public class BenchmarkMeasureCollections {
 
     @Benchmark
     public void measureArrayListDeep(Blackhole bh) {
-        for (ArrayList list : arrayLists) {
+        for (ArrayList<Person> list : arrayLists) {
             bh.consume(meter.measureDeep(list));
         }
     }
 
     @Benchmark
+    public void measureLinkedListDeep(Blackhole bh) {
+        for (LinkedList<Person> linkedList : linkedLists) {
+            bh.consume(meter.measureDeep(linkedList));
+        }
+    }
+
+    @Benchmark
     public void measureHashMapDeep(Blackhole bh) {
-        for (HashMap map : hashMaps) {
+        for (HashMap<String, Person> map : hashMaps) {
             bh.consume(meter.measureDeep(map));
         }
     }
 
+    private static void validateConfigurationOfSizeDistribution() {
+        if (numElementBoundaries.length == 0) {
+            throw new IllegalStateException("At least one boundary must be defined");
+        }
+        if (percentages.length != numElementBoundaries.length) {
+            throw new IllegalStateException("The # of percentages must match the # of boundaries");
+        }
+
+        int totalPercent = 0;
+        for (int percent : percentages) {
+            if (percent <= 0) {
+                throw new IllegalStateException("The percentages must be positive");
+            }
+            totalPercent += percent;
+        }
+        if (totalPercent != 100) {
+            throw new IllegalStateException("The percentages should add to 100");
+        }
+
+        int previousBoundary = 0;
+        for (int boundary : numElementBoundaries) {
+            if (boundary <= previousBoundary) {
+                throw new IllegalStateException("Each boundary must be greater than the previous boundary");
+            }
+            previousBoundary = boundary;
+        }
+    }
+
     private static ArrayList<Person>[] createArrayLists(Random random) {
-        ArrayList<Employer> employers = new ArrayList<>();
-        for (int i = 0; i < NUM_ELEMENTS_PER_COLLECTION; i++) {
+        // The ratio of people to employers is 10 to 1
+        int numEmployers = 1 + numElementBoundaries[numElementBoundaries.length - 1] / 10;
+        ArrayList<Employer> employers = new ArrayList<>(numEmployers);
+        for (int i = 0; i < numEmployers; i++) {
             employers.add(new Employer(createRandomName(random)));
         }
 
         ArrayList<Person>[] lists = new ArrayList[NUM_COLLECTIONS_OF_EACH_TYPE];
         for (int i = 0; i < lists.length; i++) {
-            ArrayList<Person> people = new ArrayList<>(NUM_ELEMENTS_PER_COLLECTION);
-            for (int j = 0; j < NUM_ELEMENTS_PER_COLLECTION; j++) {
+            int numElements = determineCollectionSize(random);
+
+            ArrayList<Person> people = new ArrayList<>(numElements);
+            for (int j = 0; j < numElements; j++) {
                 people.add(createRandomPerson(random, employers));
             }
             lists[i] = people;
         }
 
         return lists;
+    }
+
+    private static int determineCollectionSize(Random random) {
+        double selector = random.nextInt(100);
+
+        int minNumElements = 0;
+        int currentPercent = 0;
+        for (int i = 0; i < percentages.length; i++) {
+            currentPercent += percentages[i];
+            if (selector < currentPercent) {
+                int maxNumElements = numElementBoundaries[i];
+                return minNumElements + random.nextInt(maxNumElements - minNumElements);
+            }
+            minNumElements = numElementBoundaries[i];
+        }
+        throw new IllegalStateException("This line should never be reached");
     }
 
     private static HashMap<String, Person> mapFromNameToPerson(List<Person> people) {
